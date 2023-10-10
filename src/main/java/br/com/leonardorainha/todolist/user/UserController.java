18a405129d6e8ffe0e16c5e7ec5aa3d6b11b3c9b
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-10-10 14:15:37 -0300

    feat: working with ResponseEntity

--- src/main/java/br/com/leonardorainha/todolist/user/UserController.java
+++ src/main/java/br/com/leonardorainha/todolist/user/UserController.java
@@ -1,6 +1,7 @@
 package br.com.leonardorainha.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,19 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
+
     @Autowired
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
         if(user != null) {
-            System.out.println("Usuário já existe!");
-            return null;
+            return ResponseEntity.badRequest().body("Usuário já existe");
         }
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.ok().body(userCreated);
     }
 
 }
